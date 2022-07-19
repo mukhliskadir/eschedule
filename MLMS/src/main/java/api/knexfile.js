437c5fdb8ba6c8bfa964b96b6e2be005e32d4de0
mukhliskadir <mukhliskadir2207@gmail.com>
2022-07-19 17:27:55 +0800

    .

--- MLMS/src/main/java/api/knexfile.js
+++ MLMS/src/main/java/api/knexfile.js
@@ -1,10 +1,38 @@
 module.exports = {
   development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+  staging: {
     client: 'pg',
-    connection: 'jdbc:postgresql://ec2-3-234-131-8.compute-1.amazonaws.com/d19mjejga32und'
+    connection: {
+      database: 'jdbc:postgresql://ec2-3-234-131-8.compute-1.amazonaws.com/d19mjejga32und',
+      user:     'ocetdbspxioaak',
+      password: '046d2c84c24f70b0f1b8cf071d97fe00efe0700a42909777604ad0298b5bec3e'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: {
+      database: 'jdbc:postgresql://ec2-3-234-131-8.compute-1.amazonaws.com/d19mjejga32und',
+      user:     'ocetdbspxioaak',
+      password: '046d2c84c24f70b0f1b8cf071d97fe00efe0700a42909777604ad0298b5bec3e'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
